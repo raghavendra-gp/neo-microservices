38263592e06d254c0a877fbfd51a120014dd4873
robinrgc <robingeorgergc@gmail.com>
2023-07-21 22:43:05 +0530

    first commit

---
+++ contact_service/src/main/java/com/contact/ContactServiceApplication.java
@@ -0,0 +1,14 @@
+package com.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class ContactServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactServiceApplication.class, args);
+	}
+
+}
